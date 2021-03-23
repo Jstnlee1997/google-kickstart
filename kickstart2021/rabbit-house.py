90354ecb28d220f63ff21a386b0a97e82b0ef745
Jstnlee1997 <jstnlee1997@gmail.com>
2021-03-24 07:53:39 +0800

    git rabbit-house.py to work

--- kickstart2021/rabbit-house.py
+++ kickstart2021/rabbit-house.py
@@ -28,6 +28,25 @@ for test_case in range(T):
                 elif difference < -1:
                     total += abs(difference) -1
                     G[row][col] += abs(difference) -1
+
+    for row in reversed(range(len(G))):
+        for col in reversed(range(len(G[0]))):
+            if col -1 >= 0:
+                difference = G[row][col] - G[row][col-1]
+                if difference > 1:
+                    total += abs(difference) -1
+                    G[row][col-1] += difference - 1
+                elif difference < - 1:
+                    total += abs(difference) - 1
+                    G[row][col] += abs(difference) - 1
+            if row - 1 >= 0:
+                difference = G[row][col] - G[row-1][col]
+                if difference > 1:
+                    total += abs(difference) -1
+                    G[row-1][col] += difference -1
+                elif difference < -1:
+                    total += abs(difference) -1
+                    G[row][col] += abs(difference) -1   
     
     # print(G)
     # print(int(total))
