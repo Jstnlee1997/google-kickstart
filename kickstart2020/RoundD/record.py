372ea383a4c1f46043b6044f1524b454e2622106
Jstnlee1997 <jstnlee1997@gmail.com>
2021-04-16 22:58:31 +0800

    finalised record.py and cleared both test sets

--- kickstart2020/RoundD/record.py
+++ kickstart2020/RoundD/record.py
@@ -5,17 +5,21 @@ for test_case in range(T):
     ans = 0
     high = V[0]
 
-    # Check first element
-    if V[0] > V[1]:
-        ans +=1
-    for i in range(1,N-1):
-        if V[i] > high:
-            high = V[i]
-            if V[i] > V[i+1]:
-                ans +=1
+    if N > 1:
+        # Check first element
+        if V[0] > V[1]:
+            ans +=1
 
-    # Check for last element
-    if V[-1] > high:
-        ans +=1
+        for i in range(1,N-1):
+            if V[i] > high:
+                high = V[i]
+                if V[i] > V[i+1]:
+                    ans +=1
+
+        # Check for last element
+        if V[-1] > high:
+            ans +=1
+    else:
+        ans = 1
 
     print("Case #{}: {} ".format(test_case+1, ans))
